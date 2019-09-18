c69c25d563a489a73ea82c9ea86be8795ce73598
lwcla <18883278692@163.com>
2019-09-18 15:52:38 +0800

    日志打印添加堆栈

--- basicutil/src/main/java/com/konsung/basic/util/Debug.kt
+++ basicutil/src/main/java/com/konsung/basic/util/Debug.kt
@@ -6,14 +6,26 @@ class Debug {
 
     companion object {
 
-
-        fun info(tag: String, info: String) {
+        /**
+         * 打印日志
+         *
+         * @param tag tag
+         * @param info info
+         * @param printStack 是否打印堆栈信息
+         */
+        fun info(tag: String, info: String, printStack: Boolean = false) {
 
             if (!AppUtils.isDebug()) {
                 return
             }
 
             Log.i(tag, "lwl  $info")
+
+            if (printStack) {
+                for (i in Thread.currentThread().stackTrace) {
+                    Log.i(tag, "lwl printStack $i")
+                }
+            }
         }
     }
 }
